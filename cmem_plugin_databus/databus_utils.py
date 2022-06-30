f48b61f691345b075821349dade64b394aa86a75
Denis <yum-yab-dev@pm.me>
2022-06-30 15:42:32 +0200

    Now loads file into knowledge graph

--- cmem_plugin_databus/databus_utils.py
+++ cmem_plugin_databus/databus_utils.py
@@ -1,10 +1,53 @@
-from typing import Dict, List, Optional
-from SPARQLWrapper import SPARQLWrapper, JSON
+"""Utils for handling the DBpedia Databus"""
+
+from typing import List, Optional, Dict
+from dataclasses import dataclass
 from urllib.error import URLError
+from urllib.parse import quote
+
+import requests
+from SPARQLWrapper import SPARQLWrapper, JSON
 from cmem_plugin_base.dataintegration.types import StringParameterType, Autocompletion
 
 
-def fetch_query_result_by_key(endpoint: str, query: str, key: str) -> Optional[List[str]]:
+@dataclass
+class DatabusSearchResult:
+    typeName: str
+    score: float
+    label: str
+    resource: str
+
+
+def result_from_json_dict(json_dict: Dict[str, List[str]]) -> DatabusSearchResult:
+    return DatabusSearchResult(
+        json_dict["typeName"][0],
+        float(json_dict["score"][0]),
+        json_dict["label"][0],
+        json_dict["resource"][0],
+    )
+
+
+def fetch_api_search_result(
+    databus_base: str, query_str: str
+) -> List[DatabusSearchResult]:
+    encoded_query_str = quote(query_str)
+
+    request_uri = f"{databus_base}/api/search?query={encoded_query_str}"
+
+    json_resp = requests.get(request_uri).json()
+
+    result = []
+
+    for res in json_resp["docs"]:
+        result.append(result_from_json_dict(res))
+
+    return result
+
+
+def fetch_query_result_by_key(
+    endpoint: str, query: str, key: str
+) -> Optional[List[str]]:
+    """Sends a query to the given endpint and collects all results of a key in a list"""
     sparql_service = SPARQLWrapper(endpoint)
     sparql_service.setQuery(query)
     sparql_service.setReturnFormat(JSON)
@@ -14,7 +57,12 @@ def fetch_query_result_by_key(endpoint: str, query: str, key: str) -> Optional[L
     except URLError:
         return None
     try:
-        results = list(map(lambda binding: binding[key]["value"], query_results["results"]["bindings"]))
+        results = list(
+            map(
+                lambda binding: binding[key]["value"],
+                query_results["results"]["bindings"],
+            )
+        )
     except KeyError:
         results = []
 
@@ -22,6 +70,8 @@ def fetch_query_result_by_key(endpoint: str, query: str, key: str) -> Optional[L
 
 
 def load_accounts(sparql_endpoint: str) -> List[str]:
+    """Load available publishers from the databus.
+    Only accounts, not all publishers defined"""
     query = """PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
 PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
 PREFIX foaf: <http://xmlns.com/foaf/0.1/>
@@ -34,91 +84,112 @@ SELECT DISTINCT ?acc WHERE {
 
 
 def load_groups(sparql_endpoint: str, publisher_uri: str) -> List[str]:
-    query = ("PREFIX dct: <http://purl.org/dc/terms/>\n" +
-             "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>\n" +
-             "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>\n" +
-             "PREFIX dataid: <http://dataid.dbpedia.org/ns/core#>\n" +
-             "SELECT DISTINCT ?group WHERE {\n" +
-             "?dataset a dataid:Dataset .\n" +
-             f"?dataset dct:publisher <{publisher_uri}> .\n"
-             "?dataset dataid:group ?group .}")
+    """Load groups for a given publisher ID. CARE: #this is expected at the end!"""
+    query = (
+        "PREFIX dct: <http://purl.org/dc/terms/>\n"
+        + "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>\n"
+        + "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>\n"
+        + "PREFIX dataid: <http://dataid.dbpedia.org/ns/core#>\n"
+        + "SELECT DISTINCT ?group WHERE {\n"
+        + "?dataset a dataid:Dataset .\n"
+        + f"?dataset dct:publisher <{publisher_uri}> .\n"
+        "?dataset dataid:group ?group .}"
+    )
     return fetch_query_result_by_key(sparql_endpoint, query, "group")
 
 
 def load_artifacts(sparql_endpoint: str, group_id: str) -> List[str]:
-    query = ("PREFIX dct: <http://purl.org/dc/terms/>\n" +
-             "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>\n" +
-             "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>\n" +
-             "PREFIX dataid: <http://dataid.dbpedia.org/ns/core#>\n" +
-             "SELECT DISTINCT ?artifact WHERE {\n" +
-             f"?dataset dataid:group <{group_id}> .\n" +
-             "?dataset dataid:artifact ?artifact .}")
+    """Load artifacts for a given group ID"""
+    query = (
+        "PREFIX dct: <http://purl.org/dc/terms/>\n"
+        + "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>\n"
+        + "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>\n"
+        + "PREFIX dataid: <http://dataid.dbpedia.org/ns/core#>\n"
+        + "SELECT DISTINCT ?artifact WHERE {\n"
+        + f"?dataset dataid:group <{group_id}> .\n"
+        + "?dataset dataid:artifact ?artifact .}"
+    )
 
     return fetch_query_result_by_key(sparql_endpoint, query, "artifact")
 
 
 def load_versions(sparql_endpoint: str, artifact_id: str) -> List[str]:
-    query = ("PREFIX dct: <http://purl.org/dc/terms/>\n" +
-             "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>\n" +
-             "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>\n" +
-             "PREFIX dataid: <http://dataid.dbpedia.org/ns/core#>\n" +
-             "SELECT DISTINCT ?version WHERE {\n" +
-             f"?dataset dataid:artifact <{artifact_id}> .\n" +
-             "?dataset dataid:version ?version .}")
+    """Load versions for a given artifact ID"""
+    query = (
+        "PREFIX dct: <http://purl.org/dc/terms/>\n"
+        + "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>\n"
+        + "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>\n"
+        + "PREFIX dataid: <http://dataid.dbpedia.org/ns/core#>\n"
+        + "SELECT DISTINCT ?version WHERE {\n"
+        + f"?dataset dataid:artifact <{artifact_id}> .\n"
+        + "?dataset dataid:version ?version .}"
+    )
 
     return fetch_query_result_by_key(sparql_endpoint, query, "version")
 
 
 def load_files(sparql_endpoint: str, version_id: str) -> List[str]:
-    query = ("PREFIX dct: <http://purl.org/dc/terms/>\n" +
-             "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>\n" +
-             "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>\n" +
-             "PREFIX dataid: <http://dataid.dbpedia.org/ns/core#>\n" +
-             "PREFIX dcat: <http://www.w3.org/ns/dcat#>\n" +
-             "SELECT DISTINCT ?file WHERE {\n" +
-             f"?dataset dataid:version <{version_id}> .\n" +
-             "?dataset dcat:distribution ?dist .\n" +
-             "?dist dataid:file ?file. }")
+    """Load files for a given version ID"""
+    query = (
+        "PREFIX dct: <http://purl.org/dc/terms/>\n"
+        + "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>\n"
+        + "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>\n"
+        + "PREFIX dataid: <http://dataid.dbpedia.org/ns/core#>\n"
+        + "PREFIX dcat: <http://www.w3.org/ns/dcat#>\n"
+        + "SELECT DISTINCT ?file WHERE {\n"
+        + f"?dataset dataid:version <{version_id}> .\n"
+        + "?dataset dcat:distribution ?dist .\n"
+        + "?dist dataid:file ?file. }"
+    )
     print(query)
     return fetch_query_result_by_key(sparql_endpoint, query, "file")
 
 
 class DatabusFileAutocomplete(StringParameterType):
+    """Class for autocompleting identifiers from an arbitrary databus"""
 
     def autocomplete(
-            self, query_terms: list[str], project_id: Optional[str] = None
+        self, query_terms: list[str], project_id: Optional[str] = None
     ) -> list[Autocompletion]:
-        return self.__transform_uris_to_autocompletion(self.fetch_results(query_terms[0]))
+        return self.__transform_uris_to_autocompletion(
+            self.fetch_results_by_uri(query_terms[0])
+        )
 
     @staticmethod
-    def fetch_results(query_str: str):
+    def fetch_results_by_uri(query_str: str) -> List[str]:
+        """Fetches results for autocompletion for Databus File Identifiers and returns a list of URIs"""
 
         query_no_http = query_str.replace("https://", "")
         parts = query_no_http.rstrip("/ ").rsplit("/", 5)
 
         endpoint = "https://" + parts[0] + "/sparql"
 
-        normalized_querystr = query_str.rstrip("/ ")
+        normalized_querystr = query_str[0 : query_str.rfind("/")]
 
-        if len(parts) == 1:
-            # when its only the databus -> fetch possible accounts
-            return load_accounts(endpoint)
-        elif len(parts) == 2:
-            # when its the account return groups
-            # needs #this appended for publisher id
-            return load_groups(endpoint, normalized_querystr + "#this")
-        elif len(parts) == 3:
-            # when its a group -> return artifacts
-            return load_artifacts(endpoint, normalized_querystr)
-        elif len(parts) == 4:
-            # when its a artifact -> return versions
-            return load_versions(endpoint, normalized_querystr)
-        elif len(parts) == 5:
-            # when its a version -> return files
-            return load_files(endpoint, normalized_querystr)
+        try:
+            if len(parts) == 1:
+                # when its only the databus -> fetch possible accounts
+                return load_accounts(endpoint)
+            if len(parts) == 2:
+                # when its the account return groups
+                # needs #this appended for publisher id
+                return load_groups(endpoint, normalized_querystr + "#this")
+            if len(parts) == 3:
+                # when its a group -> return artifacts
+                return load_artifacts(endpoint, normalized_querystr)
+            if len(parts) == 4:
+                # when its a artifact -> return versions
+                return load_versions(endpoint, normalized_querystr)
+            if len(parts) == 5:
+                # when its a version -> return files
+                return load_files(endpoint, normalized_querystr)
+        except Exception:
+            pass
+
+        return [query_str]
 
     @staticmethod
     def __transform_uris_to_autocompletion(uri_list: List[str]) -> List[Autocompletion]:
-
+        """transforms a list of URIs into a list of autocompletion"""
         result = [Autocompletion(uri, uri) for uri in uri_list]
         return result
--- cmem_plugin_databus/loader.py
+++ cmem_plugin_databus/loader.py
@@ -1,11 +1,19 @@
-import io
+"""Plugin for loading one file from the databus and write it ino a dataset"""
 
+import io
 from cmem_plugin_base.dataintegration.description import Plugin, PluginParameter
 from cmem_plugin_base.dataintegration.parameter.dataset import DatasetParameterType
-from cmem_plugin_base.dataintegration.utils import write_to_dataset
+from cmem_plugin_base.dataintegration.utils import (
+    write_to_dataset,
+    setup_cmempy_super_user_access,
+    split_task_id,
+)
 from cmem_plugin_base.dataintegration.plugins import WorkflowPlugin
 import requests
 from .databus_utils import DatabusFileAutocomplete
+from .cmem_utils import post_streamed_bytesio
+from cmem.cmempy.workspace.tasks import get_task
+
 
 @Plugin(
     label="Simple Databus Loading Plugin",
@@ -15,33 +23,35 @@ This CMEM task loads a file from the defined Databus to a RDF dataset.
 """,
     parameters=[
         PluginParameter(
-            name="target_dataset",
-            label="Target Dataset",
-            description="Dateset name to save the response from the Databus",
-            param_type=DatasetParameterType()
+            name="target_graph",
+            label="Target Graph",
+            description="Graph name to save the response from the Databus.",
+            param_type=DatasetParameterType(dataset_type="eccencaDataPlatform"),
         ),
         PluginParameter(
             name="databus_file_id",
             label="Databus File ID",
             description="The Databus file id of the file to download",
-            param_type=DatabusFileAutocomplete()
+            param_type=DatabusFileAutocomplete(),
         ),
-    ]
+    ],
 )
 class SimpleDatabusLoadingPlugin(WorkflowPlugin):
+    """Implementation of loading one file from the Databus into a given dataset"""
 
-    def __init__(
-            self,
-            target_dataset: str,
-            databus_file_id: str
-    ) -> None:
+    def __init__(self, databus_file_id: str, target_graph: str) -> None:
         self.databus_file_id = databus_file_id
-        self.target_dataset = target_dataset
+        self.target_graph = target_graph
 
     def execute(self, inputs=()) -> None:
 
+        setup_cmempy_super_user_access()
+        self.log.info(f"Loading file from {self.databus_file_id}")
         resp = requests.get(self.databus_file_id, allow_redirects=True)
         if resp.status_code < 400:
-            write_to_dataset(self.target_dataset, io.BytesIO(resp.content))
+            # write_to_dataset(self.target_dataset, io.BytesIO(resp.content))
+            project_id, task_id = split_task_id(self.target_graph)
+            graph_uri = get_task(project=project_id, task=task_id)["data"]["parameters"]["graph"]["value"]
+            post_streamed_bytesio(str(graph_uri), io.BytesIO(resp.content), replace=True)
         else:
             raise FileNotFoundError
