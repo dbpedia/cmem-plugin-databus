326a743c72e739d2bfd641736b0dd9aab8358988
Denis <yum-yab-dev@pm.me>
2022-07-21 16:44:35 +0200

    New client version
    removes tests

--- tests/test_databus.py
+++ tests/test_databus.py
@@ -1,72 +1,34 @@
 """Plugin tests."""
+import pytest
+
 from cmem_plugin_databus.loader import SimpleDatabusLoadingPlugin
 from cmem_plugin_databus.databus_utils import *
+from cmem_plugin_databus.publisher import DatabusDeployPlugin
 
-
-# def test_execution():
-#     """Test plugin execution"""
-#     test_loading_accs()
-
-
-# def test_loading_accs():
-#     accs = load_accounts("https://d8lr.tools.dbpedia.org/sparql")
-#     assert len(accs) > 0
-#     for acc in accs:
-#         assert len(acc.replace("https://", "").split("/")) == 2
+# def test_plugin():
+#     plugin = SimpleDatabusLoadingPlugin(
+#         "https://d8lr.tools.dbpedia.org/jlareck/migration/stw-thesaurus-for-economics_2/1.0.10-alpha/stw-thesaurus-for-economics_2.ttl"
+#     )
 #
-#
-# def test_loading_groups():
-#     groups = load_groups("https://d8lr.tools.dbpedia.org/sparql", "https://d8lr.tools.dbpedia.org/jlareck#this")
-#     assert len(groups) > 0
-#     for group in groups:
-#         assert len(group.replace("https://", "").split("/")) == 3
-#
-#
-# def test_loading_artifacts():
-#     artifacts = load_artifacts("https://d8lr.tools.dbpedia.org/sparql",
-#                                "https://d8lr.tools.dbpedia.org/jlareck/migration")
-#     assert len(artifacts) > 0
-#     for art in artifacts:
-#         assert len(art.replace("https://", "").split("/")) == 4
-#
-#
-# def test_loading_versions():
-#     versions = load_versions("https://d8lr.tools.dbpedia.org/sparql",
-#                              "https://d8lr.tools.dbpedia.org/jlareck/migration/omwn-cow_1")
-#     assert len(versions) > 0
-#     for vers in versions:
-#         assert len(vers.replace("https://", "").split("/")) == 5
-#
-#
-# def test_loading_files():
-#     files = load_artifacts("https://d8lr.tools.dbpedia.org/sparql",
-#                            "https://d8lr.tools.dbpedia.org/jlareck/migration/omwn-cow_1/1.0.10-alpha")
-#     assert len(files) > 0
-#     for f in files:
-#         assert len(f.replace("https://", "").split("/")) == 6
+#     plugin.execute()
 
-def test_fetch_results():
 
-    accs = DatabusFileAutocomplete.fetch_results("https://d8lr.tools.dbpedia.org")
-    assert len(accs) > 0
-    for acc in accs:
-        assert len(acc.replace("https://", "").split("/")) == 2
+# def test_fetch_results():
+#     len_mappings = {
+#         "https://d8lr.tools.dbpedia.org": 2,
+#         "https://d8lr.tools.dbpedia.org/jlareck/": 3,
+#         "https://d8lr.tools.dbpedia.org/jlareck/migration/": 4,
+#         "https://d8lr.tools.dbpedia.org/jlareck/migration/omwn-cow_1/": 5,
+#         "https://d8lr.tools.dbpedia.org/jlareck/migration/omwn-cow_1/1.0.10-alpha/": 6
+#     }
+#
+#     def assert_correct_result_sizes(databus_uri: str, expected_size: int) -> None:
+#         results = DatabusFileAutocomplete.fetch_results_by_uri(databus_uri)
+#         assert len(results) > 0
+#         for result in results:
+#             assert len(result.replace("https://", "").split("/")) == expected_size
+#
+#     for uri, expected_path_size in len_mappings.items():
+#         assert_correct_result_sizes(uri, expected_path_size)
 
-    groups = DatabusFileAutocomplete.fetch_results("https://d8lr.tools.dbpedia.org/jlareck/")
-    for group in groups:
-        assert len(group.replace("https://", "").split("/")) == 3
 
-    artifacts = DatabusFileAutocomplete.fetch_results("https://d8lr.tools.dbpedia.org/jlareck/migration/")
-    assert len(artifacts) > 0
-    for art in artifacts:
-        assert len(art.replace("https://", "").split("/")) == 4
-
-    versions = DatabusFileAutocomplete.fetch_results("https://d8lr.tools.dbpedia.org/jlareck/migration/omwn-cow_1/")
-    assert len(versions) > 0
-    for vers in versions:
-        assert len(vers.replace("https://", "").split("/")) == 5
-
-    files = DatabusFileAutocomplete.fetch_results("https://d8lr.tools.dbpedia.org/jlareck/migration/omwn-cow_1/1.0.10-alpha/")
-    assert len(files) > 0
-    for f in files:
-        assert len(f.replace("https://", "").split("/")) == 6
