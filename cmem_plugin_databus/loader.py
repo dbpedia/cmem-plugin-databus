a9f94d5fe0281e359b41ad577796e2b5bc4b9a03
Denis <yum-yab-dev@pm.me>
2022-08-17 16:19:51 +0200

    Better user info for loader

--- cmem_plugin_databus/loader.py
+++ cmem_plugin_databus/loader.py
@@ -50,29 +50,42 @@ class SimpleDatabusLoadingPlugin(WorkflowPlugin):
         self.target_graph = target_graph
         self.chunk_size = chunk_size
 
+    def __get_graph_uri(self, context: ExecutionContext):
+        task_info = get_task(project=context.task.project_id(), task=self.target_graph)
+        return task_info["data"]["parameters"]["graph"]["value"]
+
     def execute(self, inputs=(), context: ExecutionContext = ExecutionContext()) -> None:
 
         setup_cmempy_super_user_access()
         self.log.info(f"Loading file from {self.databus_file_id}")
 
         data: bytearray = bytearray()
-        with requests.get(self.databus_file_id, allow_redirects=True, stream=True) as resp:
+        databus_file_resp = requests.get(self.databus_file_id, allow_redirects=True, stream=True)
+
+        if databus_file_resp.status_code > 400:
+            context.report.update(
+                ExecutionReport(operation_desc="Download Failed ❌", error=databus_file_resp.text)
+            )
+            return
+
+        with databus_file_resp as resp:
             for i, b in enumerate(resp.iter_content(chunk_size=self.chunk_size)):
                 data += bytearray(b)
                 desc = f"Downloading File {get_clock(i)}"
                 context.report.update(
-                    ExecutionReport(entity_count=i * self.chunk_size, operation="wait", operation_desc=desc)
+                    ExecutionReport(entity_count=i * self.chunk_size, operation="load", operation_desc=desc)
                 )
-        if resp.status_code < 400:
-            # write_to_dataset(self.target_dataset, io.BytesIO(resp.content))
-            project_id, task_id = split_task_id(self.target_graph)
-            graph_uri = get_task(project=project_id, task=task_id)["data"][
-                "parameters"
-            ]["graph"]["value"]
-            post_streamed_bytes(
-                str(graph_uri),
-                byte_iterator_context_update(bytes(data), context, self.chunk_size, "Uploading File"),
-                replace=True
+        graph_uri = self.__get_graph_uri(context)
+        post_resp = post_streamed_bytes(
+            str(graph_uri),
+            byte_iterator_context_update(bytes(data), context, self.chunk_size, "Uploading File"),
+            replace=True
+        )
+        if post_resp.status_code < 400:
+            context.report.update(
+                ExecutionReport(operation_desc="Upload Successful ✓")
             )
         else:
-            raise FileNotFoundError(self.databus_file_id)
+            context.report.update(
+                ExecutionReport(operation_desc="Upload Failed ❌", error=post_resp.text)
+            )
