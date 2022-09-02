ba562ecf18c81f6500870517cac1117f1355d964
Denis <yum-yab-dev@pm.me>
2022-09-02 13:14:56 +0200

    Adds one test so pytest stops failing

--- tests/test_databus.py
+++ tests/test_databus.py
@@ -1,5 +1,6 @@
 """Plugin tests."""
 import pytest
+from cmem_plugin_databus.utils import DatabusFileAutocomplete
 
 # def test_plugin():
 #     plugin = SimpleDatabusLoadingPlugin(
@@ -9,22 +10,22 @@ import pytest
 #     plugin.execute()
 
 
-# def test_fetch_results():
-#     len_mappings = {
-#         "https://d8lr.tools.dbpedia.org": 2,
-#         "https://d8lr.tools.dbpedia.org/jlareck/": 3,
-#         "https://d8lr.tools.dbpedia.org/jlareck/migration/": 4,
-#         "https://d8lr.tools.dbpedia.org/jlareck/migration/omwn-cow_1/": 5,
-#         "https://d8lr.tools.dbpedia.org/jlareck/migration/omwn-cow_1/1.0.10-alpha/": 6
-#     }
-#
-#     def assert_correct_result_sizes(databus_uri: str, expected_size: int) -> None:
-#         results = DatabusFileAutocomplete.fetch_results_by_uri(databus_uri)
-#         assert len(results) > 0
-#         for result in results:
-#             assert len(result.replace("https://", "").split("/")) == expected_size
-#
-#     for uri, expected_path_size in len_mappings.items():
-#         assert_correct_result_sizes(uri, expected_path_size)
+def test_fetch_results():
+    len_mappings = {
+        "https://d8lr.tools.dbpedia.org": 2,
+        "https://d8lr.tools.dbpedia.org/jlareck/": 3,
+        "https://d8lr.tools.dbpedia.org/jlareck/migration/": 4,
+        "https://d8lr.tools.dbpedia.org/jlareck/migration/omwn-cow_1/": 5,
+        "https://d8lr.tools.dbpedia.org/jlareck/migration/omwn-cow_1/1.0.10-alpha/": 6
+    }
+
+    def assert_correct_result_sizes(databus_uri: str, expected_size: int) -> None:
+        results = DatabusFileAutocomplete.fetch_results_by_uri(databus_uri)
+        assert len(results) > 0
+        for result in results:
+            assert len(result.replace("https://", "").split("/")) == expected_size
+
+    for uri, expected_path_size in len_mappings.items():
+        assert_correct_result_sizes(uri, expected_path_size)
 
 
